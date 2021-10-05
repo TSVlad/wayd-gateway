ac5197341f3e423c6099fa45e989f62153ba06e7
TSVlad <vladislav.kolmykov@yandex.ru>
2021-10-05 21:43:44 +0300

    Initial commit

---
+++ src/test/java/ru/tsvlad/waydgateway/WaydGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsvlad.waydgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaydGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
